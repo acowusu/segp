7f040d148c5ac1f568023eb612486d137f4d9a46
Jaiditya Khemani <51130635+Marc13000@users.noreply.github.com>
2024-01-14 19:15:20 +0000

    fixed electron build process

--- frontend/main.js
+++ frontend/main.js
@@ -1,27 +1,38 @@
 const { app, BrowserWindow } = require('electron');
-const next = require('next'); // Import the 'next' module
+const next = require('next');
 
 const dev = process.env.NODE_ENV !== 'production';
 const nextApp = next({ dev });
 const handle = nextApp.getRequestHandler();
+let mainWindow;
 
 function createWindow() {
-  const win = new BrowserWindow({
+  mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: { nodeIntegration: true, enableRemoteModule: true },
   });
-  win.loadURL(`http://localhost:3000`);
-  win.webContents.openDevTools();
+
+  const startURL = dev ? 'http://localhost:3000' : `file://${__dirname}/build/index.html`;
+
+  mainWindow.loadURL(startURL);
+  mainWindow.webContents.openDevTools();
+
+  mainWindow.on('closed', function () {
+    mainWindow = null;
+  });
 }
 
-app.whenReady().then(() => {
+app.on('ready', () => {
   nextApp.prepare().then(() => {
     createWindow();
-    app.on('activate', function () {
-      if (BrowserWindow.getAllWindows().length === 0) createWindow();
-    });
   });
+});
+
+app.on('activate', function () {
+  if (mainWindow === null) {
+    createWindow();
+  }
 });
 
 app.on('window-all-closed', function () {
