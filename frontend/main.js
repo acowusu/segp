f84c0cf12fe6586d76f337de6a71c86275d7df56
Jaiditya Khemani <51130635+Marc13000@users.noreply.github.com>
2024-01-14 02:22:21 +0000

    build: electron starts but build failing

--- frontend/main.js
+++ frontend/main.js
@@ -1,23 +1,29 @@
-const { app, BrowserWindow } = require('electron')
-const dev = process.env.NODE_ENV !== 'production'
-const nextApp = next({ dev })
-const handle = nextApp.getRequestHandler()
+const { app, BrowserWindow } = require('electron');
+const next = require('next'); // Import the 'next' module
+
+const dev = process.env.NODE_ENV !== 'production';
+const nextApp = next({ dev });
+const handle = nextApp.getRequestHandler();
+
 function createWindow() {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
-  })
-  win.loadURL(`http://localhost:3000`)
-  win.webContents.openDevTools()
+    webPreferences: { nodeIntegration: true, enableRemoteModule: true },
+  });
+  win.loadURL(`http://localhost:3000`);
+  win.webContents.openDevTools();
 }
+
 app.whenReady().then(() => {
   nextApp.prepare().then(() => {
-    createWindow()
+    createWindow();
     app.on('activate', function () {
-      if (BrowserWindow.getAllWindows().length === 0) createWindow()
-    })
-  })
-})
+      if (BrowserWindow.getAllWindows().length === 0) createWindow();
+    });
+  });
+});
+
 app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') app.quit()
-})
+  if (process.platform !== 'darwin') app.quit();
+});
