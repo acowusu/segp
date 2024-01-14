95ba48251dd34490a1cd1aec0fc7078886b2fee9
Jaiditya Khemani <51130635+Marc13000@users.noreply.github.com>
2024-01-14 02:00:19 +0000

    build: updates + base files for electron

---
+++ frontend/main.js
@@ -0,0 +1,23 @@
+const { app, BrowserWindow } = require('electron')
+const dev = process.env.NODE_ENV !== 'production'
+const nextApp = next({ dev })
+const handle = nextApp.getRequestHandler()
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+  })
+  win.loadURL(`http://localhost:3000`)
+  win.webContents.openDevTools()
+}
+app.whenReady().then(() => {
+  nextApp.prepare().then(() => {
+    createWindow()
+    app.on('activate', function () {
+      if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    })
+  })
+})
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit()
+})
