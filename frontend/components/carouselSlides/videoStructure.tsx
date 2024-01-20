40297fd645ec4ae1da0512b642e56d52aae47ad9
Matthew <mt421@ic.ac.uk>
2024-01-20 02:15:44 +0000

    feat: can drag parts

--- frontend/components/carouselSlides/videoStructure.tsx
+++ frontend/components/carouselSlides/videoStructure.tsx
@@ -4,9 +4,12 @@ import Image from 'next/image';
 import { scriptsProp } from '../carousel';
 import TextArea from '../textarea';
 import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from '../videoSlideAccordian';
+import { Responsive, WidthProvider } from 'react-grid-layout';
+import 'react-grid-layout/css/styles.css';
+import 'react-resizable/css/styles.css';
+import DraggableIcon from "../../public/draggable.svg"
 
-
-
+const ResponsiveGridLayout = WidthProvider(Responsive);
 
 
 
@@ -18,18 +21,37 @@ interface VideoStructureProps {
 }
 
 const VideoStructure: React.FC<VideoStructureProps> = ({callback, setModal, allScripts}) => {
-
-  console.log(allScripts)
-
   const [sectionIndex, setSectionIndex] = useState(0);
   const[sectionScriptChoice, setSectionScriptChoice] = useState<number[]>([]);
+
+type layout = {
+  i: string;
+  x: number;
+  y: number;
+  w: number;
+  h: number;
+}[]
+
+  const [layout, setLayout] = useState<layout>([]);
 
 
   useEffect(() => {
     if (sectionScriptChoice.length == 0) {
       setSectionScriptChoice(Array(allScripts.length).fill(0))
     }
+    const initialLayout = allScripts.map((script, index) => ({
+      i: index.toString(),
+      x: 0,
+      y: index,
+      w: 1,
+      h: 1,
+    }));
+    setLayout(initialLayout);
   }, [allScripts])
+
+  const onLayoutChange = (newLayout: layout) => {
+    setLayout(newLayout);
+  };
 
   function updateScripts(content: string, innerIndex: number) {
     console.log(sectionIndex)
@@ -46,6 +68,8 @@ const VideoStructure: React.FC<VideoStructureProps> = ({callback, setModal, allS
     setSectionScriptChoice(newArray); 
   };
 
+  const breakpoints = { lg: 1200, md: 996, sm: 768, xs: 480, xxs: 0 };
+  const cols = { lg: 1, md: 1, sm: 1, xs: 1, xxs: 1 };
 
   return (
     <div className='flex flex-row h-full border rounded-xl shadow-lg shadow-white bg-black text-white'>
@@ -67,15 +91,27 @@ const VideoStructure: React.FC<VideoStructureProps> = ({callback, setModal, allS
         </div>
       </div>
 
-      <div className='basis-3/4 h-full flex flex-col p-4 gap-8 items-center overflow-auto no-scrollbar'>
+      <div className='basis-3/4 h-full w-full flex flex-col p-4 gap-8 items-center overflow-auto no-scrollbar'>
         {
           allScripts.length != 0 &&
-          allScripts.map(({section, scripts}, index) => (
+          <ResponsiveGridLayout
+            layouts={{ lg: layout }}
+            breakpoints={breakpoints}
+            cols={cols}
+            rowHeight={300}
+            width={600}
+            onLayoutChange={onLayoutChange}
+            draggableHandle=".drag-handle"
+            className='flex flex-col items-center justify-center w-full'
+          >
+
+          {allScripts.map(({scripts}, index) => (
 
             <div 
             key={index}
             onFocus={() => setSectionIndex(index)}
-            className={`w-4/5 h-2/5 p-4 2xl:p-8 flex-shrink-0 border bg-zinc-800 rounded-lg overflow-auto no-scrollbar hover:bg-gray-800 ${index == sectionIndex && "shadow-lg shadow-blue-600 border-2 border-blue-400"}`}>
+            data-grid={layout.find(item => item.i === index.toString())}
+            className={`max-w-lg h-2/5 p-4 2xl:p-8 flex-shrink-0 border bg-zinc-800 rounded-lg overflow-auto no-scrollbar hover:bg-gray-800 ${index == sectionIndex && "shadow-lg shadow-blue-600 border-2 border-blue-400"}`}>
               <Accordion type="single" collapsible>
                 <AccordionItem value="item-1">
                   <AccordionTrigger >
@@ -98,13 +134,19 @@ const VideoStructure: React.FC<VideoStructureProps> = ({callback, setModal, allS
                     </div>
                   </AccordionContent>
                 </AccordionItem>
-              </Accordion> 
-              <blockquote contentEditable="true" onBlur={(event) => updateScripts(event.target.textContent ?? "", sectionScriptChoice[index])} suppressContentEditableWarning={true}>
-                {scripts[sectionScriptChoice[index]]}
-              </blockquote>
+              </Accordion>
+              <div className='relative'>
+                <blockquote contentEditable="true" onBlur={(event) => updateScripts(event.target.textContent ?? "", sectionScriptChoice[index])} suppressContentEditableWarning={true}>
+                  {scripts[sectionScriptChoice[index]]}
+                </blockquote>
+                <div className='drag-handle hover:cursor-move absolute right-1 bottom'>
+                  <Image src={DraggableIcon} alt='draggableIcon' width={30} height={30}/>
+                </div>
+              </div>
             </div>
 
-          ))
+          ))}
+        </ResponsiveGridLayout>
         }
         <button onClick={callback} disabled={sectionScriptChoice.some(item => item == -1)} className='border p-2 2xl:p-5 rounded-lg disabled:bg-gray-700'>
           Create Video
