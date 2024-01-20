34bb6c6f240f6c975ea0f4566437fadf5c885d06
Matthew <mt421@ic.ac.uk>
2024-01-20 18:24:37 +0000

    feat: add bins, no functionality though

--- frontend/components/carouselSlides/videoStructure.tsx
+++ frontend/components/carouselSlides/videoStructure.tsx
@@ -2,12 +2,12 @@
 import React, { TextareaHTMLAttributes, useEffect, useRef, useState } from 'react';
 import Image from 'next/image';
 import { scriptsProp } from '../carousel';
-import TextArea from '../textarea';
 import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from '../videoSlideAccordian';
 import { Responsive, WidthProvider } from 'react-grid-layout';
 import 'react-grid-layout/css/styles.css';
 import 'react-resizable/css/styles.css';
 import DraggableIcon from "../../public/draggable.svg"
+import BinIcon from "../../public/bin.svg"
 
 
 const ResponsiveGridLayout = WidthProvider(Responsive);
@@ -67,38 +67,11 @@ type layout = {
   const cols = { lg: 1, md: 1, sm: 1, xs: 1, xxs: 1 };
 
 
-  const [showDropdown, setShowDropdown] = useState(false);
-  const [dropdownCoords, setDropdownCoords] = useState({ x: 0, y: 0 });
-
-  const handleContextMenu = (event: { preventDefault: () => void; clientX: any; clientY: any; }) => {
-    event.preventDefault(); // Prevent the default right-click menu
-    setShowDropdown(true);
-    setDropdownCoords({ x: event.clientX, y: event.clientY });
-  };
-
-  const handleCloseDropdown = () => {
-    setShowDropdown(false);
-  };
-
-  const DropdownMenu = ({x, y, onClose}:{ x: number, y: number, onClose: () => void }) => {
-    const style = {
-      position: 'absolute',
-      top: `${y}px`,
-      left: `${x}px`,
-      zIndex: 1000, 
-    };
-  
-    return (
-      <ul className="bg-white rounded-xl text-black">
-        {/* List your menu items here */}
-        <li>Menu Item 1</li>
-        <li>Menu Item 2</li>
-        {/* ... other items */}
-      </ul>
-    );
-  };
-
-
+  function deleteItem(index: number) {
+    allScripts.splice(index, 1); 
+    console.log(allScripts)
+  }
+ 
 
 
   return (
@@ -110,7 +83,7 @@ type layout = {
         </h1>
         <div className='h-full overflow-auto no-scrollbar flex flex-col justify-start items-start gap-2 2xl:gap-4'>
 
-          {layout.slice().sort((a, b) => a.y - b.y).map(({i, y}, index) => (
+          {layout.slice().sort((a, b) => a.y - b.y).map(({i}, index) => (
             <button onClick={() => {setSectionIndex(Number(i));}}
             className={`text-lg 2xl:text-xl p-2 font-bold hover:underline ${Number(i) == sectionIndex && "text-blue-400"}`}
             style={{ textAlign: 'left', whiteSpace: 'normal' }}
@@ -141,46 +114,50 @@ type layout = {
           >
 
           {allScripts.map(({scripts}, index) => (
-            <div 
-            onContextMenu={handleContextMenu} 
-            key={index}
-            onFocus={() => setSectionIndex(index)}
-            onClick={() => {setSectionIndex(index); handleCloseDropdown()}}
-            data-grid={layout.find(item => item.i === index.toString())}
-            className={`max-w-2xl h-2/5 p-4 2xl:p-6 flex-shrink-0 border bg-zinc-800 rounded-lg overflow-auto no-scrollbar hover:bg-gray-800 ${index == sectionIndex && "shadow-lg shadow-blue-600 border-2 border-blue-400"}`}>
-              {showDropdown && <DropdownMenu x={dropdownCoords.x} y={dropdownCoords.y} onClose={handleCloseDropdown} />}
-              <Accordion type="single" collapsible>
-                <AccordionItem value="item-1">
-                  <AccordionTrigger >
-                  <div className="w-full flex items-center justify-end text-neutral-400 text-xs 2xl:text-sm font-normal font-['Inter'] mb-4 hover:cursor-pointer">
-                    View other Drafts
-                  </div>
-                  </AccordionTrigger>
-                  <AccordionContent>
-                    <div className='flex flex-row gap-4 2xl:gap-8 overflow-auto'>
-                      {
-                        allScripts[index].scripts.map((content, innerIndex) => (
-                          <div 
-                          key={innerIndex} 
-                          onClick={() => {updateElement(index, innerIndex); updateScripts(scripts[innerIndex], innerIndex)}}
-                          className={`hover:cursor-pointer border-2 h-[6rem] rounded-lg p-4 ${innerIndex == sectionScriptChoice[index] && "border-blue-400"} overflow-auto no-scrollbar`}>
-                            {content}
-                          </div>
-                        ))
-                      }
+            <div
+              key={index}
+              onFocus={() => setSectionIndex(index)}
+              onClick={() => {setSectionIndex(index)}}
+              data-grid={layout.find(item => item.i === index.toString())}
+              className={`max-w-2xl h-2/5 p-4 2xl:p-6 flex-shrink-0 border bg-zinc-800 rounded-lg overflow-auto no-scrollbar hover:bg-gray-800 ${index == sectionIndex && "shadow-lg shadow-blue-600 border-2 border-blue-400"}`}>
+                <Accordion type="single" collapsible>
+                  <AccordionItem value="item-1">
+                    <AccordionTrigger >
+                    <div className="w-full flex items-center justify-end text-neutral-400 text-xs 2xl:text-sm font-normal font-['Inter'] mb-4 hover:cursor-pointer">
+                      View other Drafts
                     </div>
-                  </AccordionContent>
-                </AccordionItem>
-              </Accordion>
-              <div className='relative'>
-                <blockquote contentEditable="true" onBlur={(event) => updateScripts(event.target.textContent ?? "", sectionScriptChoice[index])} suppressContentEditableWarning={true}>
-                  {scripts[sectionScriptChoice[index]]}
-                </blockquote>
-                <div className='drag-handle hover:cursor-move absolute right-1 bottom'>
-                  <Image src={DraggableIcon} alt='draggableIcon' width={30} height={30}/>
+                    </AccordionTrigger>
+                    <AccordionContent>
+                      <div className='flex flex-row gap-4 2xl:gap-8 overflow-auto'>
+                        {
+                          allScripts[index].scripts.map((content, innerIndex) => (
+                            <div 
+                            key={innerIndex} 
+                            onClick={() => {updateElement(index, innerIndex); updateScripts(scripts[innerIndex], innerIndex)}}
+                            className={`hover:cursor-pointer border-2 h-[6rem] rounded-lg p-4 ${innerIndex == sectionScriptChoice[index] && "border-blue-400"} overflow-auto no-scrollbar`}>
+                              {content}
+                            </div>
+                          ))
+                        }
+                      </div>
+                    </AccordionContent>
+                  </AccordionItem>
+                </Accordion>
+                <div className='flex flex-col justify-between h-[85%]'>
+                  <blockquote contentEditable="true" onBlur={(event) => updateScripts(event.target.textContent ?? "", sectionScriptChoice[index])} suppressContentEditableWarning={true}>
+                    {scripts[sectionScriptChoice[index]]}
+                  </blockquote>
+                  <div className='w-full flex justify-between'>
+                    <div className='hover:cursor-pointer' onClick={() => deleteItem(index)}>
+                      <Image src={BinIcon} alt='bin' width={30} height={30}/>
+                    </div>
+                    <div className='drag-handle hover:cursor-move'>
+                      <Image src={DraggableIcon} alt='draggableIcon' width={30} height={30}/>
+                    </div>
+                  </div>
                 </div>
-              </div>
             </div>
+
           ))}
         </ResponsiveGridLayout>
         }
