2bc911c061092b9713bacdf29e067b5251a8fc3a
Matthew <mt421@ic.ac.uk>
2024-01-24 11:59:07 +0000

    feat: add more to web socket

--- llama.py
+++ llama.py
@@ -12,24 +12,24 @@ connections = {}
 async def handler(websocket, path):
   client_id = str(uuid.uuid4())
   connections[client_id] = websocket
-  message = await websocket.recv()
-  print(f"Received message: {message}")
-  
-  # request_queue.put((client_id, message))
-  
-  await websocket.send("Echo: " + message)
+  while True:
+    message = await websocket.recv()
+    print(f"Received message: {message}")
+    request_queue.put((client_id, message))
+    await websocket.send("Echo: " + message)
 
-# async def response_handler():
-#   while True:
-#     client_id, script = request_queue.get()
+async def response_handler():
+  while True:
+    client_id, script = request_queue.get()
     
-#     websocket = connections[client_id]
+    websocket = connections[client_id]
     
-#     print("responding")
+    print("responding")
     
-#     if websocket:
-#       await websocket.send(script)
-#       await websocket.close()
+    if websocket:
+      await websocket.send(script)
+      await websocket.close()
+
 
 
 start_server = websockets.serve(handler, "localhost", 6789)
