fbd37b73ab4218dd24b6966ede85cd09aea1a265
Matthew <mt421@ic.ac.uk>
2024-01-27 21:15:01 +0000

    feat: can replace items

--- desktop/src/components/timeline-components/timeFrame.tsx
+++ desktop/src/components/timeline-components/timeFrame.tsx
@@ -22,27 +22,27 @@ interface TimeFrameProps {
     row: TimelineRow;
     data:additionalDataType;
     deleteItem: (id: string, rowid: string) => void;
-    setToReplace: (action: TimelineAction | null) => void;
-    toReplace: TimelineAction | null
+    setToReplace: (action: {rowid: string, action: TimelineAction} | null) => void;
+    toReplace: {rowid: string, action: TimelineAction} | null
 }
 
 export const TimeFrame: React.FC<TimeFrameProps> = ({action, row, data, deleteItem, setToReplace, toReplace}) => {
     return ( 
         <ContextMenu>
             <ContextMenuTrigger>
-            <div key={row.id} className={`flex flex-row items-center cursor-pointer border ${toReplace && toReplace.id === action.id ? "border-blue-400 border-2" : "border-white"} h-full`}>
+            <div key={row.id} className={`flex flex-row items-center cursor-pointer border ${toReplace && toReplace.action.id === action.id ? "border-blue-400 border-2" : "border-white"} h-full`}>
                 <img src={data.img} alt={action.id} className="object-cover object-center w-full h-full" />
             </div>
             </ContextMenuTrigger>
             <ContextMenuContent>
                 <ContextMenuItem onClick={() => deleteItem(action.id, row.id)}>Delete</ContextMenuItem>
                 <ContextMenuItem onClick={() => {
-                    if (toReplace && toReplace.id === action.id) {
+                    if (toReplace && toReplace.action.id === action.id) {
                         setToReplace(null)
                     } else {
-                        setToReplace(action)
+                        setToReplace({rowid: row.id, action: action})
                     }
-                    }}>{toReplace && toReplace.id === action.id ? "Deselect" : "Replace"}</ContextMenuItem>
+                    }}>{toReplace && toReplace.action.id === action.id ? "Deselect" : "Replace"}</ContextMenuItem>
                 <ContextMenuItem>Set Animation</ContextMenuItem>
                 <ContextMenuSub>
                 <ContextMenuSubTrigger>Move Layers</ContextMenuSubTrigger>
