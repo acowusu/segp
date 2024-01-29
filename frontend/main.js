1d2ac944652228523f2a1340bc3964a238a93946
Kagan Ugur Pekgoz <kup21@ic.ac.uk>
2024-01-29 17:13:32 +0000

    kup: resizing for the js file as well

--- frontend/main.js
+++ frontend/main.js
@@ -1,40 +1,42 @@
-const { app, BrowserWindow } = require('electron');
-const next = require('next');
+const { app, BrowserWindow } = require("electron");
+const next = require("next");
 
-const dev = process.env.NODE_ENV !== 'production';
+const dev = process.env.NODE_ENV !== "production";
 const nextApp = next({ dev });
 const handle = nextApp.getRequestHandler();
 let mainWindow;
 
 function createWindow() {
   mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1920,
+    height: 1080,
     webPreferences: { nodeIntegration: true, enableRemoteModule: true },
   });
 
-  const startURL = dev ? 'http://localhost:3000' : `file://${__dirname}/build/index.html`;
+  const startURL = dev
+    ? "http://localhost:3000"
+    : `file://${__dirname}/build/index.html`;
 
   mainWindow.loadURL(startURL);
   mainWindow.webContents.openDevTools();
 
-  mainWindow.on('closed', function () {
+  mainWindow.on("closed", function () {
     mainWindow = null;
   });
 }
 
-app.on('ready', () => {
+app.on("ready", () => {
   nextApp.prepare().then(() => {
     createWindow();
   });
 });
 
-app.on('activate', function () {
+app.on("activate", function () {
   if (mainWindow === null) {
     createWindow();
   }
 });
 
-app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') app.quit();
+app.on("window-all-closed", function () {
+  if (process.platform !== "darwin") app.quit();
 });
