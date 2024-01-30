ef527dd243fd00a6493c27f5678e97d89be0ca8a
Alex Owusu <Alexander.c.owusu@gmail.com>
2024-01-30 14:57:41 +0000

    Add debug configuration and enable sourcemaps

--- desktop/vitest.config.ts
+++ desktop/vitest.config.ts
@@ -2,7 +2,7 @@ import { configDefaults, defineConfig, mergeConfig } from "vitest/config";
 import viteConfig from "./vite.config.ts";
 
 export default mergeConfig(
-  viteConfig,
+  await viteConfig,
   defineConfig({
     test: {
       includeSource: ["**/*.ts", "**/*.tsx"],
