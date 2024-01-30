46b42ac9572e7d171e54fa962211db4749a75468
Alex Owusu <Alexander.c.owusu@gmail.com>
2024-01-30 00:51:16 +0000

    Add vitest configuration and update tsconfig.json

--- desktop/vitest.config.ts
+++ desktop/vitest.config.ts
@@ -1,13 +1,17 @@
-import { defineConfig, mergeConfig } from "vitest/config";
+import { configDefaults, defineConfig, mergeConfig } from "vitest/config";
 import viteConfig from "./vite.config.ts";
 
 export default mergeConfig(
   viteConfig,
   defineConfig({
     test: {
+      includeSource: ["**/*.ts", "**/*.tsx"],
+      exclude: [...configDefaults.exclude, "**/*.js"],
       coverage: {
         // you can include other reporters, but 'json-summary' is required, json is recommended
         reporter: ["text", "json-summary", "json"],
+        extension:['.ts', '.tsx'],
+        include:["src/**", "electron/**"],
         // If you want a coverage reports even if your tests are failing, include the reportOnFailure option
         reportOnFailure: true,
       },
