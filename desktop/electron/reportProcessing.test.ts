754461c201a21d7dffb988be032c6bc34ec9db50
Alex Owusu <Alexander.c.owusu@gmail.com>
2024-01-30 01:09:47 +0000

    increase coverage

--- desktop/electron/reportProcessing.test.ts
+++ desktop/electron/reportProcessing.test.ts
@@ -1,8 +1,9 @@
-import { getVisuals, getVoiceovers, getTopics, getAudiences } from "./reportProcessing";
+import { getVisuals, getVoiceovers, getTopics, getAudiences, setVoiceover, setAudience, setVisual } from "./reportProcessing";
 import voiceovers from './mockData/voiceovers.json'
 import topics from './mockData/topics.json'
 import audiences from './mockData/audiences.json'
-import { test, expect } from "vitest";
+import visuals from './mockData/visuals.json'
+import { test, expect, vi } from "vitest";
 
 test("getVoiceovers should return an array of voiceovers", async () => {
     const result = await getVoiceovers();
@@ -24,3 +25,31 @@ test("getAudiences should return an array of audiences", async () => {
     expect(result).toEqual(audiences);
 });
 
+
+
+
+test("setVoiceover should log the voiceover", async () => {
+    const consoleSpy = vi.spyOn(console, 'log');
+    
+    await setVoiceover(voiceovers[0]);
+    
+    expect(consoleSpy).toHaveBeenCalledWith(voiceovers[0]);
+});
+
+test("setAudience should log the audience", async () => {
+    const consoleSpy = vi.spyOn(console, 'log');
+    
+    const audience = audiences[0];
+    await setAudience(audience);
+    
+    expect(consoleSpy).toHaveBeenCalledWith(audience);
+});
+
+test("setVisual should set the visual", async () => {
+    const consoleSpy = vi.spyOn(console, 'log');
+    const visual = visuals[0];
+    await setVisual(visual);
+
+  
+    expect(consoleSpy).toHaveBeenCalledWith(visual);
+});
