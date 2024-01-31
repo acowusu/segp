9200bd6fe7b084a956b04928a18ebfb51764dfa0
Matthew <mt421@ic.ac.uk>
2024-01-31 11:25:09 +0000

    feat: latest version of backend

--- llama.py
+++ llama.py
@@ -1,38 +1,51 @@
 import asyncio
 import websockets
 import uuid
-from queue import Queue
+from asyncio import Queue
+import openllm
 
 
-request_queue = Queue()
+async def start_server():
+    request_queue = Queue()
+    connections = {}
+    queue_lock = asyncio.Lock()
 
-connections = {}
+    print("ghaskdhasjdg")
+
+    llm = openllm.LLM("mistralai/Mixtral-8x7B-Instruct-v0.1")
+
+    print("askjdhakjsdh")
+
+    async def handler(websocket, path):
+        client_id = str(uuid.uuid4())
+        connections[client_id] = websocket
+        try:
+            while True:
+                message = await websocket.recv()
+                print(f"Received message: {message}")
+                await request_queue.put((client_id, message))
+                await websocket.send("Echo: " + message)
+        except websockets.ConnectionClosed:
+            print(f"Client {client_id} disconnected")
+            del connections[client_id]
+
+    async def response_handler():
+        while True:
+            async with queue_lock:
+                client_id, script = await request_queue.get()
+
+                response = await llm.generate(script)
 
 
-async def handler(websocket, path):
-  client_id = str(uuid.uuid4())
-  connections[client_id] = websocket
-  while True:
-    message = await websocket.recv()
-    print(f"Received message: {message}")
-    request_queue.put((client_id, message))
-    await websocket.send("Echo: " + message)
+                print(response.outputs[0].text)
 
-async def response_handler():
-  while True:
-    client_id, script = request_queue.get()
-    
-    websocket = connections[client_id]
-    
-    print("responding")
-    
-    if websocket:
-      await websocket.send(script)
-      await websocket.close()
+                websocket = connections.get(client_id)
+                if websocket and websocket.open:
+                    print("Responding to client", client_id)
+                    await websocket.send(response.outputs[0].text)
 
+    server = await websockets.serve(handler, "0.0.0.0", 80)
+    await asyncio.gather(server.wait_closed(), response_handler())
 
-
-start_server = websockets.serve(handler, "localhost", 6789)
-
-asyncio.get_event_loop().run_until_complete(start_server)
-asyncio.get_event_loop().run_forever()
+# Run the server
+asyncio.run(start_server())
