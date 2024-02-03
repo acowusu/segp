8f72039c90265d35dfeb172e6eb90debec757c97
Kagan Ugur Pekgoz <kup21@ic.ac.uk>
2024-02-03 13:16:57 +0000

    kup: possible export problems TO FIX

--- desktop/src/pages/editor.tsx
+++ desktop/src/pages/editor.tsx
@@ -13,6 +13,7 @@ import { TimeFrame } from "../components/timeline-components/timeFrame";
 import TimelinePlayer from "../components/timeline-components/timelinePlayer";
 import { Media } from "./mediaFiles";
 import { Video } from "pexels";
+import { Button } from "../components/ui/button";
 import {
   ContextMenu,
   ContextMenuContent,
@@ -56,31 +57,33 @@ export const VideoEditor: React.FC = () => {
   const timelineState = useRef<TimelineState>(null);
   const domRef = useRef<HTMLDivElement>(null);
 
-    const [data, setData] = useState<TimelineRow[]>([]);
-    const [effects, setEffects] = useState<Record<string, TimelineEffect>>({});
-    const [allowEdit, setAllowEdit] = useState(true);
-    const [showCursor, setShowCursor] = useState(true);
-    const [autoScrollWhenPlay, setAutoScrollWhenPlay] = useState(true);
-    const idRef = useRef(5);
-    const [isPlaying, setIsPlaying] = useState(false);
-    const [selectedItem, setSelectedItem] = useState<string | null>(null)
-    const [selectedToReplace, setSelectedToReplace] = useState<{rowid: string, action: TimelineAction} | null>(null)
-    const [playbackRate, setPlaybackRate] = useState(1);
-    const [selectedMedia, setSelectedMedia] = useState<string | Video>();
+  const [data, setData] = useState<TimelineRow[]>([]);
+  const [effects, setEffects] = useState<Record<string, TimelineEffect>>({});
+  const [allowEdit, setAllowEdit] = useState(true);
+  const [showCursor, setShowCursor] = useState(true);
+  const [autoScrollWhenPlay, setAutoScrollWhenPlay] = useState(true);
+  const idRef = useRef(5);
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [selectedItem, setSelectedItem] = useState<string | null>(null);
+  const [selectedToReplace, setSelectedToReplace] = useState<{
+    rowid: string;
+    action: TimelineAction;
+  } | null>(null);
+  const [playbackRate, setPlaybackRate] = useState(1);
+  const [selectedMedia, setSelectedMedia] = useState<string | Video>();
 
+  const [isExportInProgress, setIsExportInProgress] = useState<boolean>(false);
+  const [isExportedBefore, setIsExportedBefore] = useState<boolean>(false);
 
-    const [isExportInProgress, setIsExportInProgress] = useState<boolean>(false);
-    const [isExportedBefore, setIsExportedBefore] = useState<boolean>(false);
-
-    const handlePlayPause = () => {
-      const time = timelineState.current?.getTime() ?? 0;
-      if (!isPlaying) {
-        reRenderVideo();
-      }
-      movieRef.current?.seek(time);
-      isPlaying ? movieRef.current?.pause() : movieRef.current?.play();
-      setIsPlaying(!isPlaying);
-    };
+  const handlePlayPause = () => {
+    const time = timelineState.current?.getTime() ?? 0;
+    if (!isPlaying) {
+      reRenderVideo();
+    }
+    movieRef.current?.seek(time);
+    isPlaying ? movieRef.current?.pause() : movieRef.current?.play();
+    setIsPlaying(!isPlaying);
+  };
 
   const handleAllowEdit = () => {
     setAllowEdit(!allowEdit);
@@ -95,11 +98,11 @@ export const VideoEditor: React.FC = () => {
   };
 
   const handleCursorSeek = (time: number) => {
-    if (movieRef.current) {
-      movieRef.current.pause();
+    if (mediaStore.hasMovieRef()) {
+      mediaStore.pause();
       setIsPlaying(false);
-      movieRef.current.seek(time);
-      movieRef.current.refresh();
+      mediaStore.seek(time);
+      mediaStore.refresh();
     }
   };
 
@@ -107,14 +110,13 @@ export const VideoEditor: React.FC = () => {
     time: number,
     _: React.MouseEvent<HTMLDivElement, MouseEvent>,
   ) => {
-    if (movieRef.current) {
-      reRenderVideo();
-      console.log(time);
-      movieRef.current.pause();
+    if (mediaStore.hasMovieRef()) {
+      mediaStore.pause();
       setIsPlaying(false);
-      movieRef.current.seek(time);
-      movieRef.current.refresh();
+      mediaStore.seek(time);
+      mediaStore.refresh();
     }
+
     return true;
   };
 
@@ -132,44 +134,48 @@ export const VideoEditor: React.FC = () => {
       }),
     );
 
-      data.forEach((row) => {
-        if (row.id !== "Audio") {
-          const layers = row.actions.map((action) => {
-            const img = (additionalData.find(({ id }) => action.id === id)?.additionalData || {img: ""}).img ?? ""
-            const layer = new etro.layer.Image({
-              startTime: action.start / playbackRate,
-              duration: (action.end - action.start) / playbackRate,
-              source: img,
-              sourceX: 0, // default: 0
-              sourceY: 0, // default: 0
-              sourceWidth: 1920, // default: null (full width)
-              sourceHeight: 1080, // default: null (full height)
-              x: 0, // default: 0
-              y: 0, // default: 0
-              width: 1920, // default: null (full width)
-              height: 1080, // default: null (full height)
-              opacity: 1 , // default: 1
-            })
-            mediaStore.set(action.id, layer)
-            return layer
-          })
-          mediaStore.addLayers(layers);
-        }
-      })
-      
-      mediaStore.refresh();
-      movieRef.current = mediaStore.getMovie();
-      console.log(mediaStore._actionLayerMap)
+    data.forEach((row) => {
+      if (row.id !== "Audio") {
+        const layers = row.actions.map((action) => {
+          const img =
+            (
+              additionalData.find(({ id }) => action.id === id)
+                ?.additionalData || { img: "" }
+            ).img ?? "";
+          const layer = new etro.layer.Image({
+            startTime: action.start / playbackRate,
+            duration: (action.end - action.start) / playbackRate,
+            source: img,
+            sourceX: 0, // default: 0
+            sourceY: 0, // default: 0
+            sourceWidth: 1920, // default: null (full width)
+            sourceHeight: 1080, // default: null (full height)
+            x: 0, // default: 0
+            y: 0, // default: 0
+            width: 1920, // default: null (full width)
+            height: 1080, // default: null (full height)
+            opacity: 1, // default: 1
+          });
+          mediaStore.set(action.id, layer);
+          return layer;
+        });
+        mediaStore.addLayers(layers);
+      }
+    });
 
-    }
+    mediaStore.refresh();
+    movieRef.current = mediaStore.getMovie();
+    console.log(mediaStore._actionLayerMap);
+  };
 
-    const deleteLayer = (rowid:string ) => {
-      setData((prev) => prev.filter((item) => item.id !== rowid))
-      reRenderVideo();
-    }
+  const deleteLayer = (rowid: string) => {
+    setData((prev) => prev.filter((item) => item.id !== rowid));
+    reRenderVideo();
+  };
 
-    const deleteItem = (id: string, rowid:string ) => {
-      setData((prev) => prev.map((rowdata) => {
+  const deleteItem = (id: string, rowid: string) => {
+    setData((prev) =>
+      prev.map((rowdata) => {
         if (rowdata.id === rowid) {
           return {
             ...rowdata,
@@ -179,70 +185,92 @@ export const VideoEditor: React.FC = () => {
         return rowdata;
       }),
     );
+    reRenderVideo();
   };
 
-    const createNewLayer = () => {
-      const newData = data
-      newData.splice(data.length - 1, 0, {
-        id: `Layer ${idRef.current++}`,
-        actions: [],
-        rowHeight: 150
-      })
-      setData(newData)
-      reRenderVideo();
-    }
+  const createNewLayer = () => {
+    const newData = data;
+    newData.splice(data.length - 1, 0, {
+      id: `Layer ${idRef.current++}`,
+      actions: [],
+      rowHeight: 150,
+    });
+    setData(newData);
+    reRenderVideo();
+  };
 
-    // replace next selected item or add new item to timeline
-    const handleAddNewAction = (_: React.MouseEvent<HTMLElement, MouseEvent>, param: {row: TimelineRow; time: number; }) => {
-      if (!selectedMedia) return;
-        // instead of adding directly to second row, we should add to selected row
-        // instead of adding at current timestamp add at clicked timestamp
-      setData((prev) => { 
-          const rowIndex = prev.findIndex(item => item.id === param.row.id);
-          const newAction: TimelineAction = {
-              id: `action${idRef.current++}`,
-              start: param.time,
-              end: param.time + 0.5,
-              effectId: "effect0",
-          }
-          setAdditionalData((prev) => [...prev, {id: newAction.id, rowid: param.row.id, additionalData: {img: (typeof selectedMedia === "string" ? selectedMedia : undefined), video: (typeof selectedMedia !== "string" ? selectedMedia : undefined)}}])
-          prev[rowIndex] = {...param.row, actions: param.row.actions.concat(newAction)};
-          return [...prev];
-        })
-      reRenderVideo();
-    };
-
-    const handleReplaceAction = (media: Video | string ) => {
-      if (selectedToReplace) {
-        const {action} = selectedToReplace;
-        const newData = additionalData
-        const dataIndex = newData.findIndex(({id}) => id === action.id)
-        newData[dataIndex].additionalData =  {img: (typeof media === "string" ? media : undefined), video: (typeof media !== "string" ? media : undefined)}
-        setAdditionalData(newData)
-        setSelectedToReplace(null)
-        reRenderVideo();
-      }
-    }
-
-    const saveMovieAsMp4 = async () => {
-
-      await mediaStore.getMovie()?.record({
-          frameRate: mediaStore.framerate,
-          type: "video/webm;codecs=vp9",
-          // audio: default true,
-          // video: default true,
-          // duration: default end of video
-          // onStart: optional callback
-          onStart: (_: MediaRecorder) => {
-              console.log("recording started");
-              setIsExportedBefore(true)
-              setIsExportInProgress(true);
+  // replace next selected item or add new item to timeline
+  const handleAddNewAction = (
+    _: React.MouseEvent<HTMLElement, MouseEvent>,
+    param: { row: TimelineRow; time: number },
+  ) => {
+    if (!selectedMedia) return;
+    // instead of adding directly to second row, we should add to selected row
+    // instead of adding at current timestamp add at clicked timestamp
+    setData((prev) => {
+      const rowIndex = prev.findIndex((item) => item.id === param.row.id);
+      const newAction: TimelineAction = {
+        id: `action${idRef.current++}`,
+        start: param.time,
+        end: param.time + 0.5,
+        effectId: "effect0",
+      };
+      setAdditionalData((prev) => [
+        ...prev,
+        {
+          id: newAction.id,
+          rowid: param.row.id,
+          additionalData: {
+            img: typeof selectedMedia === "string" ? selectedMedia : undefined,
+            video:
+              typeof selectedMedia !== "string" ? selectedMedia : undefined,
           },
+        },
+      ]);
+      prev[rowIndex] = {
+        ...param.row,
+        actions: param.row.actions.concat(newAction),
+      };
+      return [...prev];
+    });
+    reRenderVideo();
+  };
+
+  const handleReplaceAction = (media: Video | string) => {
+    if (selectedToReplace) {
+      const { action } = selectedToReplace;
+      const newData = additionalData;
+      const dataIndex = newData.findIndex(({ id }) => id === action.id);
+      newData[dataIndex].additionalData = {
+        img: typeof media === "string" ? media : undefined,
+        video: typeof media !== "string" ? media : undefined,
+      };
+      setAdditionalData(newData);
+      setSelectedToReplace(null);
+      reRenderVideo();
+    }
+  };
+
+  const saveMovieAsMp4 = async () => {
+    await mediaStore
+      .getMovie()
+      ?.record({
+        frameRate: mediaStore.framerate,
+        type: "video/webm;codecs=vp9",
+        // audio: default true,
+        // video: default true,
+        // duration: default end of video
+        // onStart: optional callback
+        onStart: (_: MediaRecorder) => {
+          console.log("recording started");
+          setIsExportedBefore(true);
+          setIsExportInProgress(true);
+        },
       })
       .then((blob) => {
-        const newBlob = new Blob([blob], {type: "video/mp4"})
+        const newBlob = new Blob([blob], { type: "video/mp4" });
         const url = URL.createObjectURL(newBlob);
-        const a = document.createElement('a');
+        const a = document.createElement("a");
         a.href = url;
         a.download = "video.mp4";
         // a.onabort = () => {
@@ -250,7 +278,6 @@ export const VideoEditor: React.FC = () => {
         // }
         a.click();
         setIsExportInProgress(false);
-        
       });
   };
 
@@ -317,7 +344,7 @@ export const VideoEditor: React.FC = () => {
 
     mediaStore.addLayers(mocklayers);
     mediaStore.refresh();
-    movieRef.current = mediaStore.movie;
+    movieRef.current = mediaStore.getMovie();
 
     // row.actions
     // row.classNames
@@ -328,8 +355,8 @@ export const VideoEditor: React.FC = () => {
     // data should only have 2 rows for now, the first row being the images, the second row being the audio
 
     const dummyData = [
-      { img: "./example.jpg", start: 0.0, duration: 2.1 },
-      { img: "./example2.jpg", start: 2.1, duration: 1.6 },
+      { img: "./example.jpg", start: 0.0, duration: 2 },
+      { img: "./example2.jpg", start: 2, duration: 1.6 },
       { img: "./example3.jpg", start: 4, duration: 0.4 },
     ];
 
@@ -367,59 +394,74 @@ export const VideoEditor: React.FC = () => {
       },
     ]);
     // setEffects(mediaStore.effects);
-
-    // reRenderVideo()
+    reRenderVideo();
   }, []);
 
-    return ( 
-      <>
-      <div className="w-full h-screen p-4 flex flex-col items-center border overflow-auto">
-        <div className="grid grid-cols-2 h-2/5 border">
-          <div className="border overflow-auto h-full no-scrollbar">
-            <Media
-              handleSelectMedia={setSelectedMedia}
-              handleReplaceMedia={handleReplaceAction} 
-            />
-          </div>
-          <div>
-            <img className="hidden" src="/person.png" alt="" ref={imageRef} />
-            <canvas className="w-full" ref={canvasRef} />
-          </div>
-        </div>
-          <div className='flex flex-row w-full border items-center justify-center gap-4'>
-              <TimelinePlayer
-                  handlePlayPause={handlePlayPause} 
-                  timelineState={timelineState} 
-                  autoScrollWhenPlay={autoScrollWhenPlay}
-                  handleSetRate={setPlaybackRate}
-              />
-              <button
-                className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-1 px-1 rounded-lg m-4"
-                onClick={() => {
-                  mediaStore.seek(0);
-                  console.log("export button clicked");
-                  setTimeout(() => {
-                    console.log("exporting function called");
-                    saveMovieAsMp4();
-                  }, 1000);
-                }}
-              >
-                Export Video as MP4
-              </button>
-              { isExportedBefore 
-                ? (isExportInProgress
-                  ? (<div className="text-yellow-400"> Exporting... </div>)
-                  : (<div className="text-green-400"> Export Complete </div>))
-                : ""
-              }
+  const VideoPlayer = (
+    <>
+      <img className="hidden" src="/person.png" alt="" ref={imageRef} />
+      <canvas className="w-full" ref={canvasRef} />
+      <div className="flex w-full flex-row items-center justify-center gap-4">
+        <TimelinePlayer
+          handlePlayPause={handlePlayPause}
+          timelineState={timelineState}
+          autoScrollWhenPlay={autoScrollWhenPlay}
+          handleSetRate={setPlaybackRate}
+        />
+      </div>
     </>
   );
-  const MediaUtils = <>
-    <Media 
-      handleSelectMedia={setSelectedMedia}
-      handleReplaceMedia={handleReplaceAction} 
-    />
-  </>;
+
+  // change to be a svg later
+  const exportWhiteImage: string = "../../public/export-white.png";
+
+  const EditorButtons = (
+    <>
+      <Provider>
+        <Tooltip>
+          <TooltipTrigger>
+            <Button
+              className="my-1"
+              variant={"ghost"}
+              onClick={() => {
+                mediaStore.seek(0);
+                console.log("export button clicked");
+                setTimeout(() => {
+                  console.log("exporting function called");
+                  saveMovieAsMp4();
+                }, 1000);
+              }}
+            >
+              <img src={exportWhiteImage} alt="image" width={40} height={40} />
+            </Button>
+          </TooltipTrigger>
+          <TooltipPortal>
+            <TooltipContent className="m-2 rounded-md border border-[#f7f7f8] bg-[#020817] p-2 leading-none text-[#f8fafc]">
+              Export to mp4
+              <TooltipArrow className="fill-[#f7f7f8]" />
+            </TooltipContent>
+          </TooltipPortal>
+        </Tooltip>
+      </Provider>
+      {isExportedBefore ? (
+        isExportInProgress ? (
+          <div className="text-yellow-400"> Exporting... </div>
+        ) : (
+          <div className="text-green-400"> Export Complete </div>
+        )
+      ) : (
+        ""
+      )}
+    </>
+  );
+  const MediaUtils = (
+    <>
+      <Media
+        handleSelectMedia={setSelectedMedia}
+        handleReplaceMedia={handleReplaceAction}
+      />
+    </>
+  );
 
   const TimelineTitle = (
     <div>
@@ -440,7 +482,7 @@ export const VideoEditor: React.FC = () => {
           timelineState.current?.setScrollTop(target.scrollTop);
         }}
         className=""
-      > 
+      >
         <Button className="flex" variant={"outline"} onClick={createNewLayer}>
           {" "}
           {/** NO idea why not centred */}
@@ -540,7 +582,9 @@ export const VideoEditor: React.FC = () => {
         {/* I wanna get rid of this and just use columns*/}
         {/* Media Tab */}
         {/* <div className="border overflow-auto h-full no-scrollbar"> */}
-        <div className="col-span-2 ">{EditorButtons}</div>
+        <div className="col-span-2 ">
+          <EditorButtons />
+        </div>
         <div className=" row-span-1 flex overflow-auto ">{MediaUtils}</div>
         {/* Player Component (Wraps the Canvas and the play/pause bar) */}
         <div className="row-span-1">{VideoPlayer}</div>
