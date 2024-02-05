86ef0814d679928fffa1503899566029c419deeb
Restyled.io <commits@restyled.io>
2024-02-05 20:05:59 +0000

    Restyled by prettier

--- frontend/main.js
+++ frontend/main.js
@@ -1,33 +1,39 @@
 const dev = process.env.NODE_ENV !== "production";
-const nextApp = next({dev});
+const nextApp = next({ dev });
 const handle = nextApp.getRequestHandler();
 let mainWindow;
 
 function createWindow() {
   mainWindow = new BrowserWindow({
-    width : 1920,
-    height : 1080,
-    webPreferences : {nodeIntegration : true, enableRemoteModule : true},
+    width: 1920,
+    height: 1080,
+    webPreferences: { nodeIntegration: true, enableRemoteModule: true },
   });
 
-  const startURL =
-      dev ? "http://localhost:3000" : `file://${__dirname}/build/index.html`;
+  const startURL = dev
+    ? "http://localhost:3000"
+    : `file://${__dirname}/build/index.html`;
 
   mainWindow.loadURL(startURL);
   mainWindow.webContents.openDevTools();
 
-  mainWindow.on("closed", function() { mainWindow = null; });
+  mainWindow.on("closed", function () {
+    mainWindow = null;
+  });
 }
 
-app.on("ready", () => { nextApp.prepare().then(() => { createWindow(); }); });
+app.on("ready", () => {
+  nextApp.prepare().then(() => {
+    createWindow();
+  });
+});
 
-app.on("activate", function() {
+app.on("activate", function () {
   if (mainWindow === null) {
     createWindow();
   }
 });
 
-app.on("window-all-closed", function() {
-  if (process.platform !== "darwin")
-    app.quit();
+app.on("window-all-closed", function () {
+  if (process.platform !== "darwin") app.quit();
 });
