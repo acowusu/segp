42de259ace8a8dcb8e2c491d1158fac38b86d656
Restyled.io <commits@restyled.io>
2024-02-15 20:02:15 +0000

    Restyled by black

--- server/subtitle_test/audiotosub.py
+++ server/subtitle_test/audiotosub.py
@@ -12,8 +12,8 @@ def create_srt(data):
     subtitles = ""
     start_time = 0
     for idx, clip in enumerate(data, start=1):
-        end_time = start_time + clip['duration']
-        subtitle_text = clip['text']
+        end_time = start_time + clip["duration"]
+        subtitle_text = clip["text"]
         subtitle = f"{idx}\n{seconds_to_srt_time(start_time)} --> {seconds_to_srt_time(end_time)}\n{subtitle_text}\n\n"
         subtitles += subtitle
         start_time = end_time
@@ -24,23 +24,23 @@ data = [
     {
         "audioUrl": "http://localhost:8888/audio/5802178923580196912.mp3",
         "duration": 5.296,
-        "text": "The sun was setting behind the distant mountains, casting a warm glow over the tranquil valley below."
+        "text": "The sun was setting behind the distant mountains, casting a warm glow over the tranquil valley below.",
     },
     {
         "audioUrl": "http://localhost:8888/audio/8816817326943404923.mp3",
         "duration": 3.942,
-        "text": "Birds chirped softly in the trees, and a gentle breeze rustled the leaves."
+        "text": "Birds chirped softly in the trees, and a gentle breeze rustled the leaves.",
     },
     {
         "audioUrl": "http://localhost:8888/audio/-1336862706260963644.mp3",
         "duration": 6.491,
-        "text": "As the evening descended, the sky turned into a canvas of vibrant colors, painting the horizon with shades of orange, pink, and purple."
+        "text": "As the evening descended, the sky turned into a canvas of vibrant colors, painting the horizon with shades of orange, pink, and purple.",
     },
     {
         "audioUrl": "http://localhost:8888/audio/-4072941278507644217.mp3",
         "duration": 3.942,
-        "text": "It was a peaceful scene, a moment of serenity amidst the chaos of the world."
-    }
+        "text": "It was a peaceful scene, a moment of serenity amidst the chaos of the world.",
+    },
 ]
 
 # Generate subtitles
