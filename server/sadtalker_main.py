f8ab40b3f8efa859f55c8c944c9f01d6c284bc4d
Restyled.io <commits@restyled.io>
2024-02-15 21:33:56 +0000

    Restyled by autopep8

--- server/sadtalker_main.py
+++ server/sadtalker_main.py
@@ -3,20 +3,24 @@ from typing_extensions import Annotated
 from pydantic import BaseModel
 import subprocess
 from datetime import datetime
-import uvicorn 
+import uvicorn
 from base64 import b64encode
 import os
 import glob
+
 
 class SadTalker(BaseModel):
     driven_audio: Annotated[str, Form()]
     source_image: Annotated[str, Form()]
 
+
 app = FastAPI()
+
 
 @app.get("/")
 def read_root():
     return {"msg": "Hello World"}
+
 
 @app.post("/avatar/")
 async def animate_portrait(sadtalker: SadTalker):
@@ -32,21 +36,21 @@ async def animate_portrait(sadtalker: SadTalker):
         ]
         print(' '.join(command))
         subprocess.run(command, check=True)
-        
+
         # get the last from results
         print(os.listdir(results_dir))
         results = sorted(os.listdir(results_dir))
         mp4_name = glob.glob(results_dir + '*.mp4')[0]
-        mp4 = open('{}'.format(mp4_name),'rb').read()
+        mp4 = open('{}'.format(mp4_name), 'rb').read()
         data_url = "data:video/mp4;base64," + b64encode(mp4).decode()
-        
+
         # Return data url
         return {
-                "message": "Returned animation: {}".format(mp4_name),
-                "data_url": data_url
-                }
+            "message": "Returned animation: {}".format(mp4_name),
+            "data_url": data_url
+        }
     except Exception as e:
         return {"error": str(e)}
-    
+
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8888)
--- server/test_sadtalker_main.py
+++ server/test_sadtalker_main.py
@@ -10,13 +10,14 @@ def test_read_main():
     assert response.status_code == 200
     assert response.json() == {"msg": "Hello World"}
 
+
 def test_generate_avatar():
     response = client.post(
-            "/avatar/",
-            headers = { 'Content-Type': 'application/json' },
-            json = {
-                    "driven_audio": "./examples/driven_audio/RD_Radio31_000.wavi",
-                    "source_image": "./examples/source_image/art_18.png"
-                    }
+        "/avatar/",
+        headers={'Content-Type': 'application/json'},
+        json={
+            "driven_audio": "./examples/driven_audio/RD_Radio31_000.wavi",
+            "source_image": "./examples/source_image/art_18.png"
+        }
     )
     assert response.status_code == 200
