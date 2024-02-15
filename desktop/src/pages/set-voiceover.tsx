933eda9243089bbcdaf5546f437387d31a90daa0
Alex Owusu <Alexander.c.owusu@gmail.com>
2024-02-15 21:48:44 +0000

    update voiceover

--- desktop/src/pages/set-voiceover.tsx
+++ desktop/src/pages/set-voiceover.tsx
@@ -1,6 +1,6 @@
-import React, { useState } from "react";
+import React, { useCallback, useState } from "react";
 import { Button } from "../components/ui/button";
-import { useEffect } from 'react';
+import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import {
   FramelessCard,
@@ -21,67 +21,79 @@ import {
 } from "../components/ui/select";
 import { Voiceover } from "../../electron/mockData/data";
 
-
 export const SetVoiceover: React.FC = () => {
   const [items, setItems] = useState<Voiceover[]>([]);
-  const [selectedVoiceover, setSelectedVoiceover] = useState<Voiceover>({} as Voiceover);
-  const [disabled, setDisabled] = useState(false);
-  useEffect( () => {
-    window.api.getVoiceovers().then((data) => {
-      setItems(data);
-    });
-  }, []);
- 
+  const [selectedVoiceover, setSelectedVoiceover] = useState<Voiceover>(
+    {} as Voiceover
+  );
+
   const navigate = useNavigate();
-  const setVisuals = async () => {
+  const navigateNext = async () => {
     navigate("/welcome/set-visuals");
-  }
-  const setVoiceover = async (Voiceover :Voiceover) => {
-    if (disabled) return;
-    setDisabled(true);
-    if (Voiceover !== undefined) {
-      setSelectedVoiceover(Voiceover);
-      console.log(selectedVoiceover)
-    }
-    // const path = await window.api.setTopic(items.find((topic) => topic.topic === selectedTopic) as Topic);
-    // console.log(path);
-    // setFilePath(path);
-    setDisabled(false);
   };
-   
+  const setVoiceover = useCallback(async (voiceover: Voiceover) => {
+    console.log(voiceover);
+    if (voiceover !== undefined) {
+      setSelectedVoiceover(voiceover);
+      window.api.setVoiceover(voiceover);
+      console.log(voiceover);
+    }
+  }, []);
+  useEffect(() => {
+    window.api
+      .getVoiceovers()
+      .then((data) => {
+        setItems(data);
+      })
+      .then(() =>
+        window.api
+          .getProjectVoiceover()
+          .then((data) => {
+            setVoiceover(data);
+          })
+          .catch((e) => {
+            console.log(e);
+          })
+      );
+  }, [setVoiceover]);
+
   return (
     <div className="flex items-center justify-center mt-4">
-      <FramelessCard >
+      <FramelessCard>
         <CardHeader>
           <CardTitle>Select Select the Voiceover for the video</CardTitle>
           <CardDescription>
-            The Voiceover will effect the language complexity and the tone of the video.
+            The Voiceover will effect the language complexity and the tone of
+            the video.
           </CardDescription>
         </CardHeader>
         <CardContent className="h-4/6">
-        <Select>
-      <SelectTrigger >
-        <SelectValue placeholder="Select an Voiceover" />
-      </SelectTrigger>
-      <SelectContent>
+          <Select>
+            <SelectTrigger>
+              {/* <SelectValue placeholder="Select an Voiceover" /> */}
+              <SelectValue  placeholder="Select an Voiceover"  aria-label={selectedVoiceover.name}>
+          {selectedVoiceover.name}
+        </SelectValue>
+            </SelectTrigger>
+            <SelectContent>
               <SelectGroup>
-              {items.map((item) => (
-                <SelectItem
-                  key={item.name}
-                  value={item.name}
-                  onClick={()=>setVoiceover(item)}
-                >
-                  {item.name}
-                </SelectItem>
-              ))}
-         
-        </SelectGroup>
-      </SelectContent>
-    </Select>
+                {items.map((item) => (
+                  <SelectItem
+                    key={item.name}
+                    value={item.name}
+                    onClick={() => setVoiceover(item)}
+                  >
+                    {item.name}
+                  </SelectItem>
+                ))}
+              </SelectGroup>
+            </SelectContent>
+          </Select>
+          {selectedVoiceover.name }
         </CardContent>
         <CardFooter className="flex justify-between">
           <Button variant="outline">Back</Button>
-          <Button onClick={setVisuals}>Next</Button>
+          <Button onClick={navigateNext}>Next</Button>
         </CardFooter>
       </FramelessCard>
     </div>
