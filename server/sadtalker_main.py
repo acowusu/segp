2f065ea5fc50b307016d3f25ecda940b49bf1b02
Fox4Song <liyuesong10u@gmail.com>
2024-02-15 19:37:38 +0000

    changed files path

--- server/sadtalker_main.py
+++ server/sadtalker_main.py
@@ -21,9 +21,9 @@ def read_root():
 @app.post("/avatar/")
 async def animate_portrait(sadtalker: SadTalker):
     try:
-        results_dir = "../../../www/sadtalker_results/"
+        results_dir = "../../www/sadtalker_results/"
         command = [
-            "python3", "inference.py",
+            "python3", "./SadTalker/inference.py",
             "--driven_audio", sadtalker.driven_audio,
             "--source_image", sadtalker.source_image,
             "--enhancer", "gfpgan",
