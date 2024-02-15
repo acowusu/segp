ece14eabcc8ed0db36015912f59855b44899299d
ls1221 <ls1221@ic.ac.uk>
2024-02-15 19:16:58 +0000

    Sadtalker renders on etro layer

--- desktop/src/pages/avatargen.tsx
+++ desktop/src/pages/avatargen.tsx
@@ -1,15 +1,16 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import etro from "etro"
 
 export const AvatarGenerator: React.FC = () => {
   const [sourceImage, setSourceImage] = useState<string>('');
   const [avatar, setAvatar] = useState<{id: string, imagePath: string}>({id: '', imagePath: ''});
-  const [avatarResponse, setAvatarResponse] = useState<string | null>(null);
-  const ttsMockAudioUrl = "./examples/driven_audio/RD_Radio31_000.wav";
-  const mockAvatar = {id: "1", imagePath: "./examples/source_image/art_18.png"}; 
+  const [avatarResponse, setAvatarResponse] = useState<string>('');
+  const ttsMockAudioUrl = "./examples/driven_audio/full.wav";
+  const mockAvatar = {id: "1", imagePath: "./examples/source_image/lebron-green.png"}; 
+  const canvasRef = React.useRef<HTMLCanvasElement>(null);
 
   const generateAvatar = async () => {
-    // Assuming window.api.textToAudio returns a promise with the audio URL
-    // const [url, id]= await window.api.textToAudio();
+
     try {
         const result = await window.electronAPI.generateAvatar(mockAvatar, ttsMockAudioUrl);
         setAvatarResponse(result);
@@ -17,6 +18,37 @@ export const AvatarGenerator: React.FC = () => {
         console.error('Error generating avatar:', error);
     }
   };
+
+  useEffect(() => {
+
+    if (!canvasRef.current) return;
+    const canvas = canvasRef.current;
+
+    const movie = new etro.Movie({
+      canvas: canvas,
+      repeat: true,
+      background: etro.parseColor('white'),
+    })
+
+    canvas.width = 1920;
+    canvas.height = 1080;
+
+    const avatarLayer = new etro.layer.Video({
+      startTime: 0,
+      duration: 10,
+      source: avatarResponse,
+    })
+
+    // const avatarChromaKey = new etro.effect.ChromaKey({
+    //   target: etro.parseColor('green'),
+    //   interpolate: false,
+    //   threshold: 10,
+    // })
+
+    // avatarLayer.addEffect(avatarChromaKey);
+    movie.addLayer(avatarLayer);
+    movie.play();
+  }, [avatarResponse]);
 
   return (
     <div>
@@ -32,11 +64,10 @@ export const AvatarGenerator: React.FC = () => {
     >
       Generate Avatar
     </button>
-        {avatarResponse && 
-            <video controls>
-                <source src={avatarResponse} type="video/mp4"/>
-            </video>
-        }
+      <div>
+        <canvas className="w-full" ref={canvasRef} />
+      </div>
+      {/* <video src={avatarResponse} controls /> */}
     </div>
   );
 };
