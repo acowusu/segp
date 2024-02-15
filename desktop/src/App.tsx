069122b7d1f2fe2d164332e1d01c38e50d871a3e
Alex Owusu <Alexander.c.owusu@gmail.com>
2024-02-15 23:29:27 +0000

    Remove set-audience and set-voiceover pages from welcome layout

--- desktop/src/App.tsx
+++ desktop/src/App.tsx
@@ -7,9 +7,7 @@ import { Layout } from "./components/layout";
 import { WelcomeLayout } from "./components/welcome-layout";
 import { SetVisuals } from "./pages/set-visuals";
 import { NotFound } from "./pages/not-found";
-import { SetAudience } from "./pages/set-audience";
 import { SetTopic } from "./pages/set-topic";
-import { SetVoiceover } from "./pages/set-voiceover";
 import { ScriptEditor } from "./pages/script-editor";
 import { Upload } from "./pages/upload";
 import { ThemeProvider } from "./components/theme";
@@ -26,8 +24,6 @@ function App() {
             <Route index element={<Upload />} />
             <Route path="welcome/" element={<WelcomeLayout />}>
               <Route path="set-topic" element={<SetTopic />} />
-              <Route path="set-audience" element={<SetAudience />} />
-              <Route path="set-voiceover" element={<SetVoiceover />} />
               <Route path="set-visuals" element={<SetVisuals />} />
               <Route path="script-editor" element={<ScriptEditor />} />
               <Route path="media" element={<Media />} />
