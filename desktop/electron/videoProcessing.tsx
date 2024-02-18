84d16dca066060839b8d2ce00ecb4013066f89c2
Kagan Ugur Pekgoz <kup21@ic.ac.uk>
2024-02-18 15:19:34 +0000

    kup: <refactor> comments added for webm -> mp4 func

--- desktop/electron/videoProcessing.tsx
+++ desktop/electron/videoProcessing.tsx
@@ -33,11 +33,18 @@ export async function writeBlob(outFile: PathLike): Promise<void> {
   console.log("finsihed downlaod");
 }
 
+/**
+ * Takes a webm blob buffer and transcodes it into mp4
+ * @param buff arraybuffer of the video/webm blob that should be force downlaoded/written
+ * @param outFile the name of the video file (will be within the public dir)
+ */
 export async function webmBLobToMp4(
   buff: ArrayBuffer,
   outFile: PathLike
 ): Promise<void> {
   console.log("started webm -> mp4");
+
+  // TODO: add
 
   // write the contents of the buffer to a webm file
   const webmFile = `public/${outFile}`;
