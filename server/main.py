5299769325092448ab94d9d0e73705380d4352e2
Restyled.io <commits@restyled.io>
2024-02-19 14:19:04 +0000

    Restyled by yapf

--- server/main.py
+++ server/main.py
@@ -19,20 +19,17 @@ from typing_extensions import Annotated
 model_id = "mistralai/Mixtral-8x7B-Instruct-v0.1"
 tokenizer = AutoTokenizer.from_pretrained(model_id)
 
-quantization_config = BitsAndBytesConfig(
-    load_in_4bit=True, bnb_4bit_compute_dtype=torch.float16
-)
+quantization_config = BitsAndBytesConfig(load_in_4bit=True,
+                                         bnb_4bit_compute_dtype=torch.float16)
 model = AutoModelForCausalLM.from_pretrained(
-    model_id, quantization_config=quantization_config
-)
+    model_id, quantization_config=quantization_config)
 
 app = FastAPI(root_path="/v1")
 
 
 @app.post("/generate")
-async def generate(
-    prompt: Annotated[str, Form()], temperature: Annotated[float, Form()]
-):
+async def generate(prompt: Annotated[str, Form()],
+                   temperature: Annotated[float, Form()]):
     inputs = tokenizer(prompt, return_tensors="pt").to(0)
     output = model.generate(**inputs, max_new_tokens=8192)
     json_data = tokenizer.decode(output[0], skip_special_tokens=True)
