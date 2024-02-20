44e1f3e1d194f3e95c229eedfb0c33764f97fa82
Kagan Ugur Pekgoz <kup21@ic.ac.uk>
2024-02-20 22:29:11 +0000

    kup: <refactor> removed the extra Layout import

--- desktop/src/App.tsx
+++ desktop/src/App.tsx
@@ -14,7 +14,6 @@ import { ThemeProvider } from "./components/theme";
 import { Media } from "./pages/mediaFiles";
 import { Editor } from "./pages/editor";
 import { VideoGeneratorDummy } from "./pages/videogen";
-import { Layout } from "./components/layout";
 import { LLMDebugger } from "./pages/llm-debugger";
 function App() {
   // const [count, setCount] = useState(0);
