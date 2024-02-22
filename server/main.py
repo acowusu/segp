2b28e6eb571e5f3c5ffc33dcfecc02739042f539
Restyled.io <commits@restyled.io>
2024-02-22 13:04:54 +0000

    Restyled by autopep8

--- server/main.py
+++ server/main.py
@@ -7,13 +7,14 @@ import bentoml
 from bentoml.io import Image, JSON, Multipart
 from typing_extensions import Annotated
 import uuid
-# LDA 
+# LDA
 # Bert Topic
 import uvicorn
 import json
 # llm = LLM("TheBloke/Llama-2-13B-chat-GPTQ", backend="vllm"  )
 # llm = LLM("TheBloke/Mixtral-8x7B-Instruct-v0.1-GPTQ", backend="vllm"  )
-llm = LLM(model_id="TheBloke/Mistral-7B-Instruct-v0.1-AWQ", quantization='awq', dtype='half', gpu_memory_utilization=.95, max_model_len=8192,  backend="vllm")
+llm = LLM(model_id="TheBloke/Mistral-7B-Instruct-v0.1-AWQ", quantization='awq',
+          dtype='half', gpu_memory_utilization=.95, max_model_len=8192,  backend="vllm")
 # llm = LLM("mistralai/Mixtral-8x7B-Instruct-v0.1", backend="vllm"  )
 # stable_diffusion =  bentoml.diffusers_simple.stable_diffusion.create_runner("CompVis/stable-diffusion-v1-4")
 # stable_diffusion_runner = bento_model.to_runner()
@@ -28,13 +29,16 @@ anything_v3_runner = bento_model.to_runner()
 
 svc = bentoml.Service("anything_v3", runners=[anything_v3_runner])
 
+
 @svc.api(input=JSON(), output=Image())
 def txt2img(input_data):
     images, _ = anything_v3_runner.run(**input_data)
     return images[0]
 
+
 app = FastAPI(root_path="/v1")
 svc.mount_asgi_app(app)
+
 
 @app.get("/")
 def read_root():
@@ -47,10 +51,10 @@ async def login(prompt: Annotated[str, Form()], width: Annotated[str, Form()],  
     return images[0]
 
 
-
 @app.post("/login/")
 async def login(username: Annotated[str, Form()], password: Annotated[str, Form()]):
     return {"username": username}
+
 
 @app.post("/generate")
 async def generate(prompt: Annotated[str, Form()], temperature: Annotated[float, Form()]):
@@ -60,6 +64,7 @@ async def generate(prompt: Annotated[str, Form()], temperature: Annotated[float,
     generator = llm.generate_iterator(
         prompt, request_id=request_id, n=1, temperature=temperature
     )
+
     async def streamer() -> AsyncGenerator[str, None]:
         async for request_output in generator:
             for output in request_output.outputs:
@@ -67,20 +72,20 @@ async def generate(prompt: Annotated[str, Form()], temperature: Annotated[float,
                 previous_texts[i].append(output.text)
                 yield output.text
 
-  
     async for _ in streamer():
         pass
     return "".join(previous_texts[0])
 
 
 @app.post("/instruct")
-async def generate(instructions: Annotated[str, Form()],prompt: Annotated[str, Form()], temperature: Annotated[float, Form()]):
+async def generate(instructions: Annotated[str, Form()], prompt: Annotated[str, Form()], temperature: Annotated[float, Form()]):
     request_id = f"tinyllm-{uuid.uuid4().hex}"
     previous_texts = [[]] * 1
-    prompt= f"[INST] <<SYS>>{instructions}<</SYS>>{prompt}[/INST]"
+    prompt = f"[INST] <<SYS>>{instructions}<</SYS>>{prompt}[/INST]"
     generator = llm.generate_iterator(
         prompt, request_id=request_id, n=1, temperature=temperature
     )
+
     async def streamer() -> AsyncGenerator[str, None]:
         async for request_output in generator:
             for output in request_output.outputs:
@@ -88,7 +93,6 @@ async def generate(instructions: Annotated[str, Form()],prompt: Annotated[str, F
                 previous_texts[i].append(output.text)
                 yield output.text
 
-  
     async for _ in streamer():
         pass
     return "".join(previous_texts[0])
