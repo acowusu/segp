dac7da364dabc67c6e85348813b9305eb7930a6c
Restyled.io <commits@restyled.io>
2024-02-27 12:02:57 +0000

    Restyled by yapf

--- server/sadtalker_main.py
+++ server/sadtalker_main.py
@@ -71,17 +71,17 @@ def is_valid_image_extension(path):
 async def animate_portrait(sadtalker: SadTalker):
     try:
         if not is_valid_path(sadtalker.driven_audio):
-            raise HTTPException(status_code=400, detail="Invalid driven_audio path")
+            raise HTTPException(status_code=400,
+                                detail="Invalid driven_audio path")
         if not is_valid_path(sadtalker.source_image):
-            raise HTTPException(status_code=400, detail="Invalid source_image path")
+            raise HTTPException(status_code=400,
+                                detail="Invalid source_image path")
         if not is_valid_audio_extension(sadtalker.driven_audio):
-            raise HTTPException(
-                status_code=400, detail="Invalid driven_audio extension"
-            )
+            raise HTTPException(status_code=400,
+                                detail="Invalid driven_audio extension")
         if not is_valid_image_extension(sadtalker.source_image):
-            raise HTTPException(
-                status_code=400, detail="Invalid source_image extension"
-            )
+            raise HTTPException(status_code=400,
+                                detail="Invalid source_image extension")
 
         os.chdir("/home/ls1221/segp/server/SadTalker")
         results_dir = "/www/sadtalker_results/"
