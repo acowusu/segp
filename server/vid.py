a821db795a98c58f9daaeab1a270fb621c242df8
Fox4Song <liyuesong10u@gmail.com>
2024-02-29 17:34:46 +0000

    api endpoint changed to take file upload

--- server/vid.py
+++ server/vid.py
@@ -1,10 +1,13 @@
 import bentoml
 from bentoml.io import Image, JSON
-from fastapi import FastAPI, Form
+from fastapi import FastAPI, Form, UploadFile, File
+from fastapi.responses import FileResponse
 import uvicorn
 from typing_extensions import Annotated
 from diffusers.utils import load_image, export_to_video
 import torch
+from io import BytesIO 
+import PIL
 
 MODEL_ID= "stabilityai/stable-video-diffusion-img2vid-xt"
 
@@ -14,7 +17,6 @@ bentoml.diffusers.import_model(
 )
 
 bento_model = bentoml.diffusers.get("svd-xt:latest")
-print(bento_model)
 svd_runner = bento_model.to_runner()
 svd_runner.init_local()
 
@@ -29,18 +31,24 @@ def img2vid(input_data):
 app = FastAPI(root_path="/v9")
 svc.mount_asgi_app(app)
 
-@app.get("/status")
-async def status():
-    return {"status": "ok"}
+#@app.get("/status")
+#async def status():
+#    return {"status": "ok"}
 
 @app.post("/video")
-async def generate_video(image_path: Annotated[str, Form()]):
-    image = load_image(image_path)
+async def generate_video(image_file: UploadFile = File(...)):
+    print("entered api")
+    data = await image_file.read()
+    image = PIL.Image.open(BytesIO(data))
+    image = PIL.ImageOps.exif_transpose(image)
+    image = image.convert("RGB")
+    # image = load_image(image_file)
     generator = torch.manual_seed(42)
     frames = svd_runner.run(image, decode_chunk_size=8, generator=generator)[0][0]
-    print(frames)
     path = export_to_video(frames, fps=7) # default written to temporary file
-    return {"filepath": path}
+    print(path)
+   
+    return FileResponse(path=path, media_type="video/mp4")
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8899)
