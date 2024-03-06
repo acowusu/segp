689ba09bd7aeefe506a7823d0db38fa22a918f55
Alex Owusu <Alexander.c.owusu@gmail.com>
2024-03-06 18:31:25 +0000

    Fix visual regression

--- desktop/src/components/custom/avatarFrame.tsx
+++ desktop/src/components/custom/avatarFrame.tsx
@@ -8,7 +8,7 @@ interface AvatarFrameProps {
 export const AvatarFrame: React.FC<AvatarFrameProps> = ({ isSelected, label, imagePath, onClick }) => {
     return (
       <div className={` border ${isSelected && 'border-sky-500 '} rounded-lg transform transition duration-200 ease-in-out  hover:opacity-[80%] hover:border-sky-500 hover: hover:border-dashed cursor-pointer flex flex-col items-center justify-between p-2`}>
-          <img src={imagePath} onClick={onClick} className="h-full w-full object-cover object-center mb-2  h-32 "  />
+          <img src={imagePath} onClick={onClick} className="h-full w-full object-cover object-center mb-2  h-32 aspect-square "  />
           <p className="">{label}</p>
       </div>
     );
