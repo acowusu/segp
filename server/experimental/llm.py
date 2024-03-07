2570bd8da1911cef07cf03e02e2f3a6e9113650c
Restyled.io <commits@restyled.io>
2024-03-07 22:11:07 +0000

    Restyled by black

--- server/experimental/llm.py
+++ server/experimental/llm.py
@@ -10,27 +10,33 @@ model_id = "mistralai/Mixtral-8x7B-Instruct-v0.1"
 tokenizer = AutoTokenizer.from_pretrained(model_id)
 
 quantization_config = BitsAndBytesConfig(
-    load_in_4bit=True,
-    bnb_4bit_compute_dtype=torch.float16
+    load_in_4bit=True, bnb_4bit_compute_dtype=torch.float16
 )
 model = AutoModelForCausalLM.from_pretrained(
-    model_id, quantization_config=quantization_config)
+    model_id, quantization_config=quantization_config
+)
 
 app = FastAPI(root_path="/v3")
 
 
 @app.post("/generate")
-async def generate(prompt: Annotated[str, Form()], temperature: Annotated[float, Form()], max_new_tokens: Annotated[int, Form()] = 8192):
+async def generate(
+    prompt: Annotated[str, Form()],
+    temperature: Annotated[float, Form()],
+    max_new_tokens: Annotated[int, Form()] = 8192,
+):
     try:
         inputs = tokenizer(prompt, return_tensors="pt").to(0)
         output = model.generate(
-            **inputs, temperature=temperature, max_new_tokens=max_new_tokens)
-        json_data = (tokenizer.decode(output[0], skip_special_tokens=True))
+            **inputs, temperature=temperature, max_new_tokens=max_new_tokens
+        )
+        json_data = tokenizer.decode(output[0], skip_special_tokens=True)
         return {"response": json_data}
     except torch.cuda.OutOfMemoryError:
         sys.exit(1)
     except Exception:
         raise HTTPException(status_code=500, detail="Internal server error")
+
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8893)
