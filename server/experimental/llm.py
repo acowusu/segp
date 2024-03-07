50a61aeb6aeb81cedbef84aedfab19e580de64fc
Restyled.io <commits@restyled.io>
2024-03-07 22:11:06 +0000

    Restyled by autopep8

--- server/experimental/llm.py
+++ server/experimental/llm.py
@@ -13,17 +13,20 @@ quantization_config = BitsAndBytesConfig(
     load_in_4bit=True,
     bnb_4bit_compute_dtype=torch.float16
 )
-model = AutoModelForCausalLM.from_pretrained(model_id, quantization_config=quantization_config)
+model = AutoModelForCausalLM.from_pretrained(
+    model_id, quantization_config=quantization_config)
 
 app = FastAPI(root_path="/v3")
 
+
 @app.post("/generate")
 async def generate(prompt: Annotated[str, Form()], temperature: Annotated[float, Form()], max_new_tokens: Annotated[int, Form()] = 8192):
-    try: 
+    try:
         inputs = tokenizer(prompt, return_tensors="pt").to(0)
-        output = model.generate(**inputs, temperature=temperature, max_new_tokens=max_new_tokens)
+        output = model.generate(
+            **inputs, temperature=temperature, max_new_tokens=max_new_tokens)
         json_data = (tokenizer.decode(output[0], skip_special_tokens=True))
-        return {"response":json_data}
+        return {"response": json_data}
     except torch.cuda.OutOfMemoryError:
         sys.exit(1)
     except Exception:
@@ -31,4 +34,3 @@ async def generate(prompt: Annotated[str, Form()], temperature: Annotated[float,
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8893)
-
