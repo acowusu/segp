ccc08d2690aae1bf860ccf65ee094c2c2607c097
Restyled.io <commits@restyled.io>
2024-03-07 22:11:12 +0000

    Restyled by yapf

--- server/experimental/llm.py
+++ server/experimental/llm.py
@@ -14,12 +14,10 @@ from typing_extensions import Annotated
 model_id = "mistralai/Mixtral-8x7B-Instruct-v0.1"
 tokenizer = AutoTokenizer.from_pretrained(model_id)
 
-quantization_config = BitsAndBytesConfig(
-    load_in_4bit=True, bnb_4bit_compute_dtype=torch.float16
-)
+quantization_config = BitsAndBytesConfig(load_in_4bit=True,
+                                         bnb_4bit_compute_dtype=torch.float16)
 model = AutoModelForCausalLM.from_pretrained(
-    model_id, quantization_config=quantization_config
-)
+    model_id, quantization_config=quantization_config)
 
 app = FastAPI(root_path="/v3")
 
@@ -32,9 +30,9 @@ async def generate(
 ):
     try:
         inputs = tokenizer(prompt, return_tensors="pt").to(0)
-        output = model.generate(
-            **inputs, temperature=temperature, max_new_tokens=max_new_tokens
-        )
+        output = model.generate(**inputs,
+                                temperature=temperature,
+                                max_new_tokens=max_new_tokens)
         json_data = tokenizer.decode(output[0], skip_special_tokens=True)
         return {"response": json_data}
     except torch.cuda.OutOfMemoryError:
