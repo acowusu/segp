3e2dc2d21560013e5301c31bf5dddde2ddfa6841
ls1221 <ls1221@ic.ac.uk>
2024-03-08 21:16:50 +0000

    fix tests again

--- desktop/src/pages/test.tsx
+++
@@ -1,7 +0,0 @@
-export const Test: React.FC = () => {
-    return (
-        <div>
-            <video src={`http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4`} controls/>
-        </div>
-    );
-}
