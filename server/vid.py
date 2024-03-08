73d2487473d8bdf92f537b76697ef41555d0f3ab
Restyled.io <commits@restyled.io>
2024-03-08 21:37:31 +0000

    Restyled by whitespace

--- server/vid.py
+++ server/vid.py
@@ -34,7 +34,7 @@ svc = bentoml.Service("svd-xt", runners=[svd_runner])
 def img2vid(input_data):
     """
 
-    :param input_data: 
+    :param input_data:
 
     """
     res = svd_runner.run(**input_data)
